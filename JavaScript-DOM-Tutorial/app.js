ca10d4b40b6e3f529d7fc6e026ce46fa883c6348
G.Charkiewicz <charkiewicz.grzegorz@gmail.com>
2020-08-05 21:58:58 +0200

    Examples of use: getElementsByClassName and getElementsByTagName

--- JavaScript-DOM-Tutorial/app.js
+++ JavaScript-DOM-Tutorial/app.js
@@ -1,4 +1,31 @@
-const search = document.getElementById('search-books');
-const bookList = document.getElementById('book-list');
+var titles = document.getElementsByClassName('title');
 
-console.log(search, bookList);
+console.log( titles[0] );
+console.log( titles[1] );
+
+
+console.log(titles[0].textContent);
+console.log(titles[1].textContent);
+
+console.log('===================== From Loop ====================');
+var loopTiteles = function(){
+    for(var i = 0; i < titles.length; i++){
+        console.log(titles[i].textContent);
+    }
+};
+
+loopTiteles();
+
+
+var lis = document.getElementsByTagName('li');
+var deleteElement = lis[0].children[1].textContent;
+console.log(deleteElement);
+
+
+console.log('===================== From Array ====================');
+console.log(Array.isArray(titles));
+console.log(Array.isArray(Array.from(titles)));
+
+Array.from(titles).forEach(function(title){
+    console.log(title);
+});
