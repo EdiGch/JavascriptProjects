60f16457817361956acb74745722196e3d1c1fb8
G.Charkiewicz <charkiewicz.grzegorz@gmail.com>
2020-08-09 16:24:46 +0200

    The Query Selector.

--- JavaScript-DOM-Tutorial/app.js
+++ JavaScript-DOM-Tutorial/app.js
@@ -1,31 +1,9 @@
-var titles = document.getElementsByClassName('title');
+const wrap = document.querySelector('#wrapper');
 
-console.log( titles[0] );
-console.log( titles[1] );
+var el = document.querySelector('#book-list > ul > li:nth-child(2) > span.name');
 
+var span  = document.querySelectorAll('#book-list li .name');
 
-console.log(titles[0].textContent);
-console.log(titles[1].textContent);
-
-console.log('===================== From Loop ====================');
-var loopTiteles = function(){
-    for(var i = 0; i < titles.length; i++){
-        console.log(titles[i].textContent);
-    }
-};
-
-loopTiteles();
-
-
-var lis = document.getElementsByTagName('li');
-var deleteElement = lis[0].children[1].textContent;
-console.log(deleteElement);
-
-
-console.log('===================== From Array ====================');
-console.log(Array.isArray(titles));
-console.log(Array.isArray(Array.from(titles)));
-
-Array.from(titles).forEach(function(title){
-    console.log(title);
-});
+Array.from(span).forEach(function (name) {
+       console.log(name);
+})
